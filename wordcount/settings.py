c86f00305098141fd255dfe89d521c9b40744fa0
pawan <pawan.pk980@gmail.com>
2021-01-29 16:15:38 +0530

    allowed host

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k!%3%x=&0ezuadb0&yb--xm(yr*9)7ruro1+cxesu9h_a33mcn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
